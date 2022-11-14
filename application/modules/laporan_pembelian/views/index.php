5483f1992c34202f4169705d980b50e376fd0f62
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-11-14 10:17:39 +0700

    update

--- application/modules/laporan_pembelian/views/index.php
+++ application/modules/laporan_pembelian/views/index.php
@@ -1196,7 +1196,6 @@
             $('#filter_date_g').daterangepicker({
                 autoUpdateInput: false,
 				showDropdowns : true,
-                singleDatePicker: true,
                 locale: {
                     format: 'DD-MM-YYYY'
                 },
@@ -1209,8 +1208,9 @@
                     'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
                 }
             });
+
             $('#filter_date_g').on('apply.daterangepicker', function(ev, picker) {
-                $(this).val(picker.startDate.format('01-01-2021') + ' - ' + picker.endDate.format('DD-MM-YYYY'));
+                $(this).val(picker.startDate.format('DD-MM-YYYY') + ' - ' + picker.endDate.format('DD-MM-YYYY'));
                 TableDate5();
             });
 
@@ -1264,7 +1264,6 @@
             $('#filter_date_hutang_penerimaan').daterangepicker({
                 autoUpdateInput: false,
 				showDropdowns : true,
-                singleDatePicker: true,
                 locale: {
                     format: 'DD-MM-YYYY'
                 },
@@ -1277,8 +1276,9 @@
                     'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
                 }
             });
+
             $('#filter_date_hutang_penerimaan').on('apply.daterangepicker', function(ev, picker) {
-                $(this).val(picker.startDate.format('01-01-2021') + ' - ' + picker.endDate.format('DD-MM-YYYY'));
+                $(this).val(picker.startDate.format('DD-MM-YYYY') + ' - ' + picker.endDate.format('DD-MM-YYYY'));
                 TableHutangPenerimaan();
             });
 
