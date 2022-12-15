d89eb9ee9b61d1ea9c3d42848aa83cbbaf5912f8
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-12-15 13:16:35 +0700

    update

--- application/modules/admin/views/login.php
+++ application/modules/admin/views/login.php
@@ -81,7 +81,21 @@
 	            </div>
 	        </div>
 	        <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
-	    </div>
+	    
+			<div class="box">
+				<div class="tomorrow"
+					data-location-id=""
+					data-language="ID"
+					data-unit-system="METRIC"
+					data-skin="light"
+					data-widget-type="aqiMini"
+					style="padding-bottom:15px;position:relative;"
+					>
+				</div>
+			</div>
+
+			
+		</div>
 	</div>
 	<!--BASIC scripts-->
 	<!-- ========================================================= -->
@@ -146,6 +160,22 @@
               }
           });
       });
-  </script>
+  	</script>
+  	<script>
+        (function(d, s, id) {
+            if (d.getElementById(id)) {
+                if (window.__TOMORROW__) {
+                    window.__TOMORROW__.renderWidget();
+                }
+                return;
+            }
+            const fjs = d.getElementsByTagName(s)[0];
+            const js = d.createElement(s);
+            js.id = id;
+            js.src = "https://www.tomorrow.io/v1/widget/sdk/sdk.bundle.min.js";
+
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', 'tomorrow-sdk');
+    </script>
 	</body>
 </html>
