2919477219f8ae6767fe5ea516b7d20429768ff9
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-01-04 09:44:29 +0700

    update

--- application/modules/laporan_penjualan/views/cetak_monitoring_piutang.php
+++ application/modules/laporan_penjualan/views/cetak_monitoring_piutang.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	  <title>MONITORING HUTANG</title>
+	  <title>MONITORING PIUTANG</title>
 	  
 	  <style type="text/css">
 		table tr.table-judul{
@@ -46,8 +46,9 @@
 		<table width="98%" border="0" cellpadding="15">
 			<tr>
 				<td width="100%" align="center">
-					<div style="display: block;font-weight: bold;font-size: 11px;">Monitoring Hutang</div>
-				    <div style="display: block;font-weight: bold;font-size: 11px;">Divisi Beton Proyek Bendungan TEMEF</div>
+					<div style="display: block;font-weight: bold;font-size: 11px;">MONITORING PIUTANG</div>
+				    <div style="display: block;font-weight: bold;font-size: 11px;">DIVISI STONE CRUSHER</div>
+				    <div style="display: block;font-weight: bold;font-size: 11px;">PT. BIA BUMI JAYENDRA</div>
 					<?php
 					function tgl_indo($date2){
 						$bulan = array (
@@ -74,40 +75,33 @@
 						
 					}
 					?>
-					<div style="display: block;font-weight: bold;font-size: 11px;">Per <?= tgl_indo(date($date2)); ?></div>
+					<div style="display: block;font-weight: bold;font-size: 11px; text-transform: uppercase;">PER <?= tgl_indo(date($date2)); ?></div>
 				</td>
 			</tr>
 		</table>	
 		<table cellpadding="2" width="98%">
 			<tr class="table-judul">
-				<th width="3%" align="center" rowspan="2" style="vertical-align:middle;">NO.</th>
-				<th width="7%" align="center">REKANAN</th>
-				<th width="7%" align="center">NOMOR</th>
-				<th width="7%" align="center">TANGGAL</th>
-				<th width="7%" align="center">TANGGAL</th>
+				<th width="5%" align="center" rowspan="2" style="vertical-align:middle;">NO.</th>
+				<th width="14%" align="center">REKANAN</th>
+				<th width="7%" align="center" rowspan="2" style="vertical-align:middle;">NO. INV</th>
+				<th width="7%" align="center" rowspan="2" style="vertical-align:middle;">TGL. INV</th>
 				<th width="17%" align="center" colspan="3">TAGIHAN</th>
-				<th width="22%" align="center" colspan="4">PEMBAYARAN</th>
-				<th width="17%" align="center" colspan="3">SISA HUTANG</th>
-				<th width="14%" align="center" colspan="3">STATUS HUTANG</th>
+				<th width="17%" align="center" colspan="3">PEMBAYARAN</th>
+				<th width="17%" align="center" colspan="3">SISA PIUTANG</th>
+				<th width="8%" align="center" rowspan="2" style="vertical-align:middle;">STATUS</th>
+				<th width="8%" align="center" rowspan="2" style="vertical-align:middle;">UMUR</th>
 			</tr>
 			<tr class="table-judul">
 				<th align="center">KETERANGAN</th>
-				<th align="center">TAGIHAN</th>
-				<th align="center">TAGIHAN</th>
-				<th align="center">VERIFIKASI</th>
 				<th align="center">DPP</th>
 				<th align="center">PPN</th>
 				<th align="center">JUMLAH</th>
 				<th align="center">DPP</th>
 				<th align="center">PPN</th>
-				<th align="center">PPH</th>
 				<th align="center">JUMLAH</th>
 				<th align="center">DPP</th>
 				<th align="center">PPN</th>
 				<th align="center">JUMLAH</th>
-				<th align="center">STATUS</th>
-				<th align="center">UMUR</th>
-				<th align="center">JATUH TEMPO</th>
 			</tr>		
             <?php   
             if(!empty($data)){
@@ -115,7 +109,7 @@
             		?>
             		<tr class="table-baris1-bold">
             			<td align="center"><?php echo $key + 1;?></td>
-            			<td align="left" colspan="17"><?php echo $row['name'];?></td>
+            			<td align="left" colspan="15"><?php echo $row['name'];?></td>
             		</tr>
 					<?php
 					$jumlah_dpp_tagihan = 0;
@@ -123,11 +117,10 @@
 					$jumlah_jumlah_tagihan = 0;
 					$jumlah_dpp_pembayaran = 0;
 					$jumlah_ppn_pembayaran = 0;
-					$jumlah_pph_pembayaran = 0;
 					$jumlah_jumlah_pembayaran = 0;
-					$jumlah_dpp_sisa_hutang = 0;
-					$jumlah_ppn_sisa_hutang = 0;
-					$jumlah_jumlah_sisa_hutang = 0;
+					$jumlah_dpp_sisa_piutang = 0;
+					$jumlah_ppn_sisa_piutang = 0;
+					$jumlah_jumlah_sisa_piutang = 0;
             		foreach ($row['mats'] as $mat) {
             			?>
 					<tr class="table-baris1">
@@ -135,20 +128,17 @@
 						<td align="left"><?php echo $mat['subject'];?></td>
 						<td align="center"><?php echo $mat['nomor_invoice'];?></td>
             			<td align="center"><?php echo $mat['tanggal_invoice'];?></td>
-            			<td align="left"><?php echo $mat['tanggal_lolos_verifikasi'];?></td>
             			<td align="right"><?php echo $mat['dpp_tagihan'];?></td>
 						<td align="right"><?php echo $mat['ppn_tagihan'];?></td>
 						<td align="right"><?php echo $mat['jumlah_tagihan'];?></td>
 						<td align="right"><?php echo $mat['dpp_pembayaran'];?></td>
 						<td align="right"><?php echo $mat['ppn_pembayaran'];?></td>
-						<td align="right"><?php echo $mat['pph_pembayaran'];?></td>
 						<td align="right"><?php echo $mat['jumlah_pembayaran'];?></td>
-						<td align="right"><?php echo $mat['dpp_sisa_hutang'];?></td>
-						<td align="right"><?php echo $mat['ppn_sisa_hutang'];?></td>
-						<td align="right"><?php echo $mat['jumlah_sisa_hutang'];?></td>
-						<td align="center"><?php echo $mat['status'];?></td>
+						<td align="right"><?php echo $mat['dpp_sisa_piutang'];?></td>
+						<td align="right"><?php echo $mat['ppn_sisa_piutang'];?></td>
+						<td align="right"><?php echo $mat['jumlah_sisa_piutang'];?></td>
+						<td align="center"><?php echo $mat['status_pembayaran'];?></td>
 						<td align="center"><?php echo $mat['syarat_pembayaran'];?></td>
-						<td align="center"><?php echo $mat['jatuh_tempo'];?></td>
             		</tr>
 
 					<?php
@@ -157,26 +147,23 @@
 					$jumlah_jumlah_tagihan += str_replace(['.', ','], ['', '.'], $mat['jumlah_tagihan']);
 					$jumlah_dpp_pembayaran += str_replace(['.', ','], ['', '.'], $mat['dpp_pembayaran']);
 					$jumlah_ppn_pembayaran += str_replace(['.', ','], ['', '.'], $mat['ppn_pembayaran']);
-					$jumlah_pph_pembayaran += str_replace(['.', ','], ['', '.'], $mat['pph_pembayaran']);
 					$jumlah_jumlah_pembayaran += str_replace(['.', ','], ['', '.'], $mat['jumlah_pembayaran']);
-					$jumlah_dpp_sisa_hutang += str_replace(['.', ','], ['', '.'], $mat['dpp_sisa_hutang']);
-					$jumlah_ppn_sisa_hutang += str_replace(['.', ','], ['', '.'], $mat['ppn_sisa_hutang']);
-					$jumlah_jumlah_sisa_hutang += str_replace(['.', ','], ['', '.'], $mat['jumlah_sisa_hutang']);
+					$jumlah_dpp_sisa_piutang += str_replace(['.', ','], ['', '.'], $mat['dpp_sisa_piutang']);
+					$jumlah_ppn_sisa_piutang += str_replace(['.', ','], ['', '.'], $mat['ppn_sisa_piutang']);
+					$jumlah_jumlah_sisa_piutang += str_replace(['.', ','], ['', '.'], $mat['jumlah_sisa_piutang']);
 					}	
 					?>
 					<tr class="table-baris2-bold">
-						<td align="right" colspan="5">JUMLAH</td>
+						<td align="right" colspan="4">JUMLAH</td>
 						<td align="right"><?php echo number_format($jumlah_dpp_tagihan,0,',','.');?></td>
 						<td align="right"><?php echo number_format($jumlah_ppn_tagihan,0,',','.');?></td>
 						<td align="right"><?php echo number_format($jumlah_jumlah_tagihan,0,',','.');?></td>
 						<td align="right"><?php echo number_format($jumlah_dpp_pembayaran,0,',','.');?></td>
 						<td align="right"><?php echo number_format($jumlah_ppn_pembayaran,0,',','.');?></td>
-						<td align="right"><?php echo number_format($jumlah_pph_pembayaran,0,',','.');?></td>
 						<td align="right"><?php echo number_format($jumlah_jumlah_pembayaran,0,',','.');?></td>
-						<td align="right"><?php echo number_format($jumlah_dpp_sisa_hutang,0,',','.');?></td>
-						<td align="right"><?php echo number_format($jumlah_ppn_sisa_hutang,0,',','.');?></td>
-						<td align="right"><?php echo number_format($jumlah_jumlah_sisa_hutang,0,',','.');?></td>
-						<td align="center"></td>
+						<td align="right"><?php echo number_format($jumlah_dpp_sisa_piutang,0,',','.');?></td>
+						<td align="right"><?php echo number_format($jumlah_ppn_sisa_piutang,0,',','.');?></td>
+						<td align="right"><?php echo number_format($jumlah_jumlah_sisa_piutang,0,',','.');?></td>
 						<td align="center"></td>
 						<td align="center"></td>
             		</tr>
@@ -185,24 +172,22 @@
             }else {
             	?>
             	<tr>
-            		<td width="100%" colspan="18" align="center">NO DATA</td>
+            		<td width="100%" colspan="15" align="center">NO DATA</td>
             	</tr>
             	<?php
             }
             ?>
             <tr class="table-total">
-				<th align="right" colspan="5">TOTAL</th>
+				<th align="right" colspan="4">TOTAL</th>
 				<th align="right"><?php echo number_format($total_dpp_tagihan,0,',','.');?></th>
 				<th align="right"><?php echo number_format($total_ppn_tagihan,0,',','.');?></th>
 				<th align="right"><?php echo number_format($total_jumlah_tagihan,0,',','.');?></th>
 				<th align="right"><?php echo number_format($total_dpp_pembayaran,0,',','.');?></th>
 				<th align="right"><?php echo number_format($total_ppn_pembayaran,0,',','.');?></th>
-				<th align="right"><?php echo number_format($total_pph_pembayaran,0,',','.');?></th>
 				<th align="right"><?php echo number_format($total_jumlah_pembayaran,0,',','.');?></th>
-				<th align="right"><?php echo number_format($total_dpp_sisa_hutang,0,',','.');?></th>
-				<th align="right"><?php echo number_format($total_ppn_sisa_hutang,0,',','.');?></th>
-				<th align="right"><?php echo number_format($total_jumlah_sisa_hutang,0,',','.');?></th>
-				<td align="center"></td>
+				<th align="right"><?php echo number_format($total_dpp_sisa_piutang,0,',','.');?></th>
+				<th align="right"><?php echo number_format($total_ppn_sisa_piutang,0,',','.');?></th>
+				<th align="right"><?php echo number_format($total_jumlah_sisa_piutang,0,',','.');?></th>
 				<td align="center"></td>
 				<td align="center"></td>
             </tr>   
