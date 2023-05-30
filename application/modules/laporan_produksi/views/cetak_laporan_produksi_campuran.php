70552f434dc98e8543e6339e033f04823a666389
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-05-30 15:29:50 +0700

    update

--- application/modules/laporan_produksi/views/cetak_laporan_produksi_campuran.php
+++ application/modules/laporan_produksi/views/cetak_laporan_produksi_campuran.php
@@ -192,7 +192,7 @@
 						<?php
 							$create = $this->db->select('*')
 							->from('akumulasi')
-							->where("(date_akumulasi between '$start_date' and '$end_date')")
+							->where("(date_akumulasi = '$end_date')")
 							->order_by('id','desc')->limit(1)
 							->get()->row_array();
 
