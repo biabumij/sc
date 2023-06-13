5abe6c2c8a703116d088e46e5c5fb2d5b7ac0a59
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-06-13 15:54:04 +0700

    update

--- application/modules/laporan/controllers/Laporan.php
+++ application/modules/laporan/controllers/Laporan.php
@@ -2340,7 +2340,14 @@ class Laporan extends Secure_Controller {
 		$pdf->setPrintFooter(true);
         $tagvs = array('div' => array(0 => array('h' => 0, 'n' => 0), 1 => array('h' => 0, 'n'=> 0)));
 		$pdf->setHtmlVSpace($tagvs);
-		        $pdf->AddPage('L');
+		
+		// add a page
+		$pdf->AddPage('L');
+		$pdf->setPrintHeader(false);
+		$pdf->setPrintFooter(false);
+		$pdf->SetY(45);
+		$pdf->SetX(6);
+		$pdf->SetMargins(10, 10);
 
 		$arr_date = $this->input->get('filter_date');
 		if(empty($arr_date)){
