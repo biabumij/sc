a919df962a287720b4a5a2ce1eed779119f48bd2
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-07-06 11:35:35 +0700

    update

--- application/modules/penjualan/views/penagihan_penjualan.php
+++ application/modules/penjualan/views/penagihan_penjualan.php
@@ -42,8 +42,8 @@
                                 <div class="panel-content">
                                     <table class="table table-striped table-bordered" width="100%">
                                         <tr>
-                                            <th width="20%" align="left">Pelanggan</th>
-                                            <th width="80%" align="left"><label class="label label-default" style="font-size:14px;"><?= $clients['nama'] ?></label>
+                                            <th width="30%" align="left">Pelanggan</th>
+                                            <th width="70%" align="left"><label class="label label-default" style="font-size:14px;"><?= $clients['nama'] ?></label>
                                             <input type="hidden" name="client_id" value="<?= $query['client_id']; ?>">
                                             <input type="hidden" name="pelanggan" value="<?= $clients['nama'] ?>">
                                             <input type="hidden" name="jenis_pekerjaan" value="<?= $sales['jobs_type']; ?>">
@@ -65,8 +65,8 @@
                                     </table>
                                     <table class="table table-striped table-bordered" width="100%">
                                         <tr>
-                                            <th width="20%" align="left">Tanggal Invoice<span class="required" aria-required="true">*</span></th>
-                                            <th width="80%" align="left"> <input type="date" class="form-control" name="tanggal_invoice" id="tanggal_invoice" required=""/></th>
+                                            <th width="30%" align="left">Tanggal Invoice<span class="required" aria-required="true">*</span></th>
+                                            <th width="70%" align="left"> <input type="date" class="form-control" name="tanggal_invoice" id="tanggal_invoice" required=""/></th>
                                         </tr>
                                         <tr>
                                             <th>Nomor Invoice<span class="required" aria-required="true">*</span></th>
