508ce1cc640f8b0dac95839f326d7f1703f49a23
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-07-25 11:50:47 +0700

    update

--- application/modules/pmm/views/setting_production.php
+++ application/modules/pmm/views/setting_production.php
@@ -24,7 +24,7 @@
                 <div class="col-sm-12 col-lg-12">
                    <div class="panel">
                         <div class="panel-header">
-                            <h3 class="section-subtitle">Setting Your Production</h3>
+                            <h3 class="section-subtitle">Perusahaan</h3>
                         </div>
                         <div class="panel-content">
                             <form id="inline-validation" method="POST" class="form-horizontal form-stripe form-submit" novalidate="novalidate" data-button="#btn-submit" action="<?php echo site_url('pmm/setting_production');?>" data-redirect="<?php echo site_url('admin/perusahaan');?>" enctype="multipart/form-data">
@@ -32,39 +32,14 @@
                                 $sp = $this->db->get_where('pmm_setting_production',array('id'=>1))->row_array();
                                 ?>
                                 <input type="hidden" name="id" value="1"></input>
-                                <div class="form-group">
-                                    <label class="col-sm-2 control-label">Factor Lost</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control numberformat" name="factor_lost" value="<?php echo number_format($sp['factor_lost'],2,',','.');?>"></input>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="col-sm-2 control-label">Content Weight</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control numberformat" name="content_weight" value="<?php echo number_format($sp['content_weight'],2,',','.');?>"></input>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="col-sm-2 control-label">Overhead %</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="overhead" value="<?php echo $sp['overhead'];?>"></input>
-                                    </div>
-                                </div>
-                                <hr />
-                                <div class="form-group">
-                                    <label class="col-sm-2 control-label">Nama Perusahaan</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="nama_pt" value="<?php echo $sp['nama_pt'];?>"></input>
-                                    </div>
-                                </div>
-                                <div class="form-group">
+                                <!--<div class="form-group">
                                     <label class="col-sm-2 control-label">Code Prefix</label>
                                     <div class="col-sm-10">
                                         <input type="text" class="form-control" name="code_prefix" value="<?php echo $sp['code_prefix'];?>"></input>
                                     </div>
-                                </div>
+                                </div>-->
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Jenis Usaha</label>
+                                    <label class="col-sm-2 control-label">Unit Bisnis</label>
                                     <div class="col-sm-10">
                                         <input type="text" class="form-control" name="jenis_usaha" value="<?php echo $sp['jenis_usaha'];?>"></input>
                                     </div>
@@ -73,7 +48,7 @@
                                     <label class="col-sm-2 control-label">Kop Surat</label>
                                     <div class="col-sm-10">
                                         <input type="file" name="kop_surat" class="form-control" data-required="false" />
-                                        <small><a href="<?= base_url();?>uploads/pmm_kop.png" target="_blank">Liat contoh</a></small>
+                                        <small><a href="<?= base_url();?>uploads/kop_surat/kop_sc.png" target="_blank">Lihat</a></small>
                                     </div>
                                     <div class="col-sm-10 col-sm-offset-2">
                                         <?php
