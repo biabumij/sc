d29a269d530e66ab4b660498005e55df2bdc74d2
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-08-11 14:13:14 +0700

    update

--- application/modules/pmm/controllers/Finance.php
+++ application/modules/pmm/controllers/Finance.php
@@ -38,7 +38,7 @@ class Finance extends CI_Controller {
 				// $row['coa'] = '' 
 				$row['saldo'] = 0;
 				if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 6 || $this->session->userdata('admin_group_id') == 15){
-					$row['action'] = '<a href="javascript:void(0);" onclick="DeleteData('.$row['id'].')" class="btn btn-danger"><i class="fa fa-close"></i> </a> <a href="javascript:void(0);" onclick="OpenForm('.$row['id'].')" class="btn btn-primary"><i class="fa fa-edit"></i> </a>';
+					$row['action'] = '<a href="javascript:void(0);" onclick="OpenForm('.$row['id'].')" class="btn btn-primary"><i class="fa fa-edit"></i> </a> <a href="javascript:void(0);" onclick="DeleteData('.$row['id'].')" class="btn btn-danger"><i class="fa fa-close"></i> </a>';
 				}else {
 				$row['actions'] = '-';
 				}
