f37828ef192c2cd3c7c67af509b334850b5f929f
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-08-23 14:53:55 +0700

    update

--- application/modules/pmm/controllers/Request_materials.php
+++ application/modules/pmm/controllers/Request_materials.php
@@ -300,9 +300,9 @@ class Request_materials extends CI_Controller {
 				
 			}else if($type == 2){
 				$arr = array('status'=>'REJECTED');
-				$this->session->set_flashdata('notif_success','Berhasil menolak Permintaan !!');
+				$this->session->set_flashdata('notif_error','<b>Data Gagal Disimpan</b>');
 			}else {
-				$this->session->set_flashdata('notif_success','Berhasil menambahkan Permintaan !!');
+				$this->session->set_flashdata('notif_success','<b>Data Berhasil Disimpan</b>');
 				$arr = array('status'=>'WAITING');
 			}
 
