df620186c0240b6b9cbde600674651a8ef299b82
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-08-23 15:17:16 +0700

    update

--- application/modules/pembelian/views/view_pembayaran_pembelian.php
+++ application/modules/pembelian/views/view_pembayaran_pembelian.php
@@ -12,14 +12,12 @@
     </style>
 </head>
 
-<body>
+<<body>
     <div class="wrap">
-
-        <?php echo $this->Templates->PageHeader(); ?>
-
+        <?php echo $this->Templates->PageHeader();?>
         <div class="page-body">
-            <?php echo $this->Templates->LeftBar(); ?>
-            <div class="content" style="padding:0;">
+            <?php echo $this->Templates->LeftBar();?>
+            <div class="content">
                 <div class="content-header">
                     <div class="leftside-content-header">
                         <ul class="breadcrumbs">
