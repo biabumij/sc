04057a1835fea5550bbd5dae5560dc18d0ff9d78
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-09-19 09:19:40 +0700

    update

--- application/modules/penjualan/views/detailPenagihan.php
+++ application/modules/penjualan/views/detailPenagihan.php
@@ -346,7 +346,7 @@
                                     <div role="tabpanel" class="tab-pane active" id="menu1">
                                         <br>
                                         <div class="table-responsive">
-                                            <table class="table table-striped table-hover table-center table-bordered" id="table-surat-jalan" width="100%">
+                                            <table class="table table-striped table-hover" id="table-surat-jalan" width="100%">
                                                 <thead>
                                                     <tr>
                                                         <th>Tanggal</th>
@@ -389,7 +389,7 @@
                                     <div role="tabpanel" class="tab-pane" id="menu2">
                                         <br>
                                         <div class="table-responsive">
-                                            <table class="table table-striped table-hover table-bordered" id="table-pembayaran" width="100%">
+                                            <table class="table table-striped table-hover" id="table-pembayaran" width="100%">
                                                 <thead>
                                                     <tr>
                                                         <th>Tanggal</th>
