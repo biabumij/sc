7fed005e3074613e7de23d8323f72cf85c14861b
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-09-20 14:37:57 +0700

    update

--- application/modules/beban_pokok_produksi/views/cetak_beban_pokok_produksi.php
+++ application/modules/beban_pokok_produksi/views/cetak_beban_pokok_produksi.php
@@ -804,7 +804,7 @@
 						<?php
 							$create = $this->db->select('id, unit_head, logistik, admin')
 							->from('akumulasi_bahan_baku')
-							->where("(date_akumulasi = '$end_date')")
+							->where("(date_akumulasi <= '$end_date')")
 							->order_by('id','desc')->limit(1)
 							->get()->row_array();
 
