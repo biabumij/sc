6c22cc877dc3459d54e4b124eb4e1a9ab8756974
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-09-20 14:41:45 +0700

    update

--- application/modules/laporan_ev_produksi/views/cetak_evaluasi_nilai_persediaan.php
+++ application/modules/laporan_ev_produksi/views/cetak_evaluasi_nilai_persediaan.php
@@ -1489,7 +1489,7 @@
 					<?php
 						$create = $this->db->select('*')
 						->from('akumulasi_bahan_baku')
-						->where("(date_akumulasi <= '$end_date')")
+						->where("date_akumulasi between '$start_date' and '$end_date'")
 						->order_by('id','desc')->limit(1)
 						->get()->row_array();
 
