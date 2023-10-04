222aa7722824fc313cef27efdc59b997db27b567
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-10-04 12:23:27 +0700

    update

--- application/modules/admin/views/login.php
+++ application/modules/admin/views/login.php
@@ -89,7 +89,7 @@
 					data-location-id="057045"
 					data-language="ID"
 					data-unit-system="METRIC"
-					data-skin="dark"
+					data-skin="light"
 					data-widget-type="aqiMini"
 					style="padding-bottom:15px;position:relative;"
 					>
