2c6d19885360bf39eba9f1ac1781207f75d58951
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-10-24 10:46:04 +0700

    update

--- application/modules/pmm/models/Pmm_model.php
+++ application/modules/pmm/models/Pmm_model.php
@@ -4315,9 +4315,9 @@ class Pmm_model extends CI_Model {
         $output = array();
 		
         $this->db->select('pmp.client_id, pmp.tanggal_pembayaran, pmp.nomor_transaksi, ppp.tanggal_invoice, ppp.nomor_invoice, pmp.total as penerimaan');
-		$this->db->join('pmm_penagihan_penjualan ppp','pmp.sales_po_id = ppp.id','left');
-        $this->db->join('pmm_sales_po ppo', 'ppp.penagihan_id = ppo.id','left');
-
+		$this->db->join('pmm_penagihan_penjualan ppp','pmp.penagihan_id = ppp.id','left');
+        $this->db->join('pmm_sales_po ppo', 'ppp.sales_po_id = ppo.id','left');
+        
 		if(!empty($start_date) && !empty($end_date)){
             $this->db->where('pmp.tanggal_pembayaran >=',$start_date);
             $this->db->where('pmp.tanggal_pembayaran <=',$end_date);
