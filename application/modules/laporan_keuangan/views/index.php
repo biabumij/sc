94d28a961ad6631169045a783772f92e79bb10dc
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-12-21 16:38:33 +0700

    update

--- application/modules/laporan_keuangan/views/index.php
+++ application/modules/laporan_keuangan/views/index.php
@@ -134,6 +134,7 @@
             locale: {
               format: 'DD-MM-YYYY'
             },
+            maxDate: new Date(2023, 09, 31),
             ranges: {
                'Today': [moment(), moment()],
                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
