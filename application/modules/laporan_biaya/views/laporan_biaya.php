29f977bcec00c76fefa8b26e17b96cff14bcf095
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-01-24 16:29:34 +0700

    update

--- application/modules/laporan_biaya/views/laporan_biaya.php
+++ application/modules/laporan_biaya/views/laporan_biaya.php
@@ -65,7 +65,7 @@
               format: 'DD-MM-YYYY'
             },
             minDate: new Date(2021, 01, 27), 
-			maxDate: new Date(2023, 07, 31),
+			maxDate: new Date(2023, 06, 31),
             ranges: {
                'Today': [moment(), moment()],
                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
